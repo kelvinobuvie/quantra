9d6e02c24e76496f968078ae6d45ab627a7b15aa
kelvinobuvie <kelvinobuvie@gmail.com>
2025-03-20 10:12:23 +0100

    download statement to the buttons

--- BlueTiles/src/Pages/Wallet/TransactionList.jsx
+++ BlueTiles/src/Pages/Wallet/TransactionList.jsx
@@ -231,7 +231,7 @@ const TransactionList = () => {
           onClick={generatePDF}
           className="bg-green-600 text-white px-6 py-2 rounded-lg"
         >
-          Download PDF
+          Download Statement
         </button>
       </div>
 
--- BlueTiles/src/component/SafeLockSummary.jsx
+++ BlueTiles/src/component/SafeLockSummary.jsx
@@ -173,7 +173,7 @@ const SafeLockSummary = ({ balance, addBalance }) => {
               onClick={generatePDF}
               className="bg-green-600 text-white px-6 py-2 rounded-lg"
             >
-              Download PDF
+              Download Statement
             </button>
           </div>
 
